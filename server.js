b4d7398e53eae42a41f48821d7475979c15fb544
Matt Trifilo <matt.trifilo@gmail.com>
2016-09-02 14:07:59 -0500

    change About section content, and remove unnecessary test route

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ app.use(compression());
 
 app.use('/', express.static(__dirname + '/public'));
 
-app.get('/test', (req, res) => {
-    res.end('<h1>IT WORKS!</h1>');
-});
-
 app.listen(4000, () => {
     console.log('Express app listening on port 4000!');
 });
