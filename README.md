3094b2dd7ae77b636350401c1e9ca332b4bf4e62
Matt Trifilo <matt.trifilo@gmail.com>
2020-08-30 12:17:42 -0700

    fix layout, links, README

--- README.md
+++ README.md
@@ -1,25 +1,3 @@
 # MattTrifilo.com
 
-Welcome to Matttrifilocom. 
-
-This is Matttrifilocom.
-
-Welcome.
-
-This is Matttrifilocom, welcome to Matttrifilocom.
-
-You can do anything at Matttrifilocom.
-
-Anything at all...
-
-The only limit is yourself...
-
-Welcome to Matttrifilocom.
-
-This is Matttrifilocom.
-
-Welcome to Matttrifilocom, welcome.
-
-THIS is Matttrifilocom.
-
-Yes... This is Matttrifilocom.
+You can do anything at Matttrifilocom. The only limit is yourself.
--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,34 @@ class App extends React.Component<Props> {
     return (
       <>
         <div className="app_layout">
-          <header className="app_header">
-            <h1>Matt Trifilo</h1>
-          </header>
+          <section className="app_headerSection">
+            <header className="app_header">
+              <h1>Matt Trifilo</h1>
+            </header>
 
-          <nav className="app_nav">
-            <a>Software</a>
+            <nav className="app_nav">
+              {/* <a>Software</a>
 
-            <a>Photography</a>
+              <a>Photography</a> */}
 
-            <a>Blog</a>
+              <a href="https://blog.matttrifilo.com">Blog</a>
 
-            <a>Linkedin</a>
-          </nav>
+              <a href="https://www.linkedin.com/in/matttrifilo/">Linkedin</a>
+            </nav>
+          </section>
+
+          <section className="app_contentSection">
+            <div className="app_aboutContent">
+              <h2>👋 I'm Matt</h2>
+
+              <p>I make things with Java, JavaScript, and occasionally Go.</p>
+
+              <p>
+                When I'm not wrangling code, I enjoy music, photography,
+                writing, and the outdoors.
+              </p>
+            </div>
+          </section>
         </div>
       </>
     );
